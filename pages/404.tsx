58263d380ec04fb503cc49adee48a1a6ca793d6d
Sebastian Ojeda <sebastian.ojeda38@gmail.com>
2021-10-15 11:17:10 -0600

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,42 @@
+import { css } from "@emotion/react";
+import Head from "next/head";
+import Link from "next/link";
+import Header from "../components/header";
+
+const Styles = {
+  title: css`
+    display: inline-block;
+    font-weight: bold;
+  `,
+  blurb: css`
+    margin-bottom: 3.5rem;
+    line-height: 1.5rem;
+    color: var(--color-accent-5);
+    a {
+      color: var(--color-success);
+      :hover {
+        color: var(--color-success-dark);
+      }
+    }
+  `,
+};
+
+export default function NotFound() {
+  return (
+    <>
+      <Head>
+        <title>Page Not Found</title>
+      </Head>
+      <Header>
+        <h1 css={Styles.title}>Page Not Found</h1>
+      </Header>
+      <p css={Styles.blurb}>
+        That page does not exist but you can explore the{" "}
+        <Link href="/blog" passHref>
+          <a>Blog</a>
+        </Link>
+        .
+      </p>
+    </>
+  );
+}
