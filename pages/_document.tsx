ea0879dec563ba570343b22bc70bc16675f7c0df
Sebastian Ojeda <me@sebastianojeda.com>
2022-01-20 16:59:55 -0700

    add inter var font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,16 +7,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-            rel="stylesheet"
-          />
+          <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
