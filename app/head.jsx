0343b9380137fbe0c77589b1f42380937940431f
Sebastian Ojeda <sebastian.ojeda38@gmail.com>
2023-01-31 22:26:38 -0700

    fix code syntax colors and responsive tag

--- app/head.jsx
+++ app/head.jsx
@@ -6,6 +6,9 @@ import Meta from "./components/Meta";
 export default function RootHead() {
   return (
     <>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
