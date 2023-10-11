cc8d8f81140693393230cd275f97166fcb1e3248
Sebastian Ojeda <sebastian.ojeda38@gmail.com>
2023-10-11 17:32:10 -0600

    prerender

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,8 +11,6 @@
 	import '../app.css';
 
 	inject({ mode: dev ? 'development' : 'production' });
-
-    export const prerender = true;
 </script>
 
 <div class="flex flex-col h-full max-w-prose mx-auto px-4">
