fa18875f1f5e750b4182ec22b37a9995d9a301fb
Sebastian Ojeda <sebastian.ojeda38@gmail.com>
2023-10-26 14:27:40 -0600

    null check

--- src/routes/views/+server.ts
+++ src/routes/views/+server.ts
@@ -3,6 +3,7 @@ import { kv } from '@vercel/kv';
 
 export async function GET({ url }) {
 	const slug = url.searchParams.get('slug');
-	const views = (await kv.get<number>(`views:${slug}`)) ? await kv.incr(`views:${slug}`) : 0;
+	const views =
+		(await kv.get<number>(`views:${slug}`)) !== null ? await kv.incr(`views:${slug}`) : 0;
 	return json({ views });
 }
