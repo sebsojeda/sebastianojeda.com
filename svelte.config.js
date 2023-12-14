a928ad51ac13146413126c00f478a72b09022168
Sebastian Ojeda <sebastian.ojeda38@gmail.com>
2023-12-13 18:41:46 -0700

    fix: add relative image tags

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,14 @@ import rehypeFigure from '@microflash/rehype-figure';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
+import relativeImages from 'mdsvex-relative-images';
 import rehypeKatexSvelte from 'rehype-katex-svelte';
 import remarkMath from 'remark-math';
 
 /** @type {import('mdsvex').MDSvexOptions */
 const mdsvexConfig = {
 	extensions: ['.md'],
-	remarkPlugins: [remarkMath],
+	remarkPlugins: [remarkMath, relativeImages],
 	rehypePlugins: [rehypeFigure, rehypeKatexSvelte]
 };
 
