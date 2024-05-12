048c19027e808572b6dee3b17e204e3ab791254e
Sebastian Ojeda <sebastian.ojeda38@gmail.com>
2024-05-12 17:57:30 -0600

    task: update linkedin

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,6 @@ export const title = 'Sebastian Ojeda';
 export const description =
 	'Sebastian Ojeda is a software engineer and computer scientist. He is currently working at Charles Schwab.';
 export const twitter = 'sebsojeda';
-export const linkedin = 'sebastian-ojeda';
+export const linkedin = 'sebsojeda';
 export const url = 'https://www.sebastianojeda.com';
 export const email = 'me@sebastianojeda.com';
